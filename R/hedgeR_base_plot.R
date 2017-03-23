fdffebc0740275221855ae892258a32ef8c751fd
Sleire <sleire@gmail.com>
2017-03-24 00:12:57 +0100

    Base plot with grid

--- R/hedgeR_base_plot.R
+++ R/hedgeR_base_plot.R
@@ -3,7 +3,7 @@
 bt <- o14
 
 # test data
-hed <- bt@Results$HedgeRate
+hed <- bt@Results$HedgeRate*100
 date <- bt@Results$Date
 mar <- bt@Results$Price
 tar <- bt@Results$Target
@@ -23,25 +23,36 @@ plot(date,
      mar,
      type="l",
      col = "gray",
+     yaxt = "n",
      xlab="",
-     ylab="Price",
+     ylab="",
      #ylim=c(10,100),
      main = "Strategy plot in base R")
 #par(bg = "grey")
 rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = "grey90")
 grid(nx = NULL, col = "white", lty =1)
+
+par(new=TRUE)
+plot(date,hed,type="l",col="lightsteelblue3",
+     ylim = c(0,100), xaxt="n",yaxt="n",xlab="",ylab="")
+polygon(c(date[1],date,tail(date, 1)),
+        c(0,hed,0),col='lightsteelblue3', border = NA)
+par(new=TRUE)
 lines(date, mar, col = "indianred")
 lines(date,tar,col="black", lty =2)
 lines(date,por,col="steelblue")
-
+axis(2)
 par(new=TRUE)
-plot(date,hed*100,type="l",col="lightsteelblue3",
+plot(date,hed,type="l",col="lightsteelblue3",
      ylim = c(0,100), xaxt="n",yaxt="n",xlab="",ylab="")
+polygon(c(date[1],date,tail(date, 1)),
+         c(0,hed,0),col='lightsteelblue3', border = NA)
+#polygon(date,hed,col='lightsteelblue3', border = NA)
 axis(4)
 mtext("Hedge %",side=4,line=3)
 #legend("topleft",col=c("red","blue"),lty=1,legend=c("y1","y2"))
 
-add_legend("topright",
+add_legend("topleft",
            legend=c("Market", "Target", "Portfolio", "Hedge"),
            pch=20,
            col=c("indianred","black", "steelblue", "lightsteelblue3"),
@@ -49,4 +60,42 @@ add_legend("topright",
 
 # for hedge area plot
 # https://github.com/VictoriaLynn/plotting-examples/blob/master/fill_between/fill_between.R
+# polygon(x,c(0,y[2:(length(y)-1)],0),col='skyblue')
 
+
+
+par(mar=c(5,4,4,5)+.1)
+plot(date,
+     hed,
+     type="l",
+     col = "gray",
+     yaxt = "n",
+     xlab="",
+     ylab="",
+     ylim=c(0,100),
+     main = "Strategy plot in base R")
+axis(4)
+mtext("Hedge %",side=4,line=3)
+# polygon(c(date[1],date,tail(date, 1)),
+#         c(0,hed,0),col='lightsteelblue3', border = NA)
+#par(bg = "grey")
+# rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = "grey90")
+# grid(nx = NULL, col = "white", lty =1)
+
+polygon(c(date[1],date,tail(date, 1)),
+        c(0,hed,0),col='lightsteelblue3', border = NA)
+#grid(nx = NULL, col = "white", lty =1)
+par(new=TRUE)
+plot(date, mar, col = "indianred", type = "l", ylab = "Price",xlab = "")
+lines(date,tar,col="black", lty =2)
+lines(date,por,col="steelblue")
+grid(nx = NULL)
+
+
+plot(date,hed,type="l",col="lightsteelblue3",
+     ylim = c(0,100), xaxt="n",yaxt="n",xlab="",ylab="")
+
+#polygon(date,hed,col='lightsteelblue3', border = NA)
+axis(4)
+mtext("Hedge %",side=4,line=3)
+#legend("topleft",col=c("red","blue"),lty=1,legend=c("y1","y2"))
