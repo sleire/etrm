d5b713b1c2490a0e94c2ba7d981cdb82a85f7781
sleire <sleire@gmail.com>
2020-09-16 11:14:05 +0200

    add dataset documentation

---
+++ R/data.R
@@ -0,0 +1,69 @@
+#' Closing prices for spread contracts at trading date 2016-12-30
+#'
+#' A synthetic dataset containing the closing prices and other attributes of 8
+#'  spread contracts. The variables are as follows:
+#'
+#' @format A data frame with 8 rows and 5 columns:
+#' \describe{
+#'   \item{Include}{boolean variable to determine if contract should be included in forward curve calculation}
+#'   \item{Contract}{the name of the futures contract}
+#'   \item{Start}{delivery start date for the futures contract}
+#'   \item{End}{delivery start date for the futures contract}
+#'   \item{Closing}{the futures contract closing price}
+#' }
+"spreads161230"
+
+
+#' Closing prices for power futures contracts at trading date 2014-09-09
+#'
+#' A synthetic dataset containing the closing prices and other attributes of 36
+#'  power futures contracts. The variables are as follows:
+#'
+#' @format A data frame with 36 rows and 5 columns:
+#' \describe{
+#'   \item{Include}{boolean variable to determine if contract should be included in forward curve calculation}
+#'   \item{Contract}{the name of the futures contract}
+#'   \item{Start}{delivery start date for the futures contract}
+#'   \item{End}{delivery start date for the futures contract}
+#'   \item{Closing}{the futures contract closing price}
+#' }
+"powfutures140909"
+
+
+#' Closing prices for power futures contracts at trading date 2015-13-13
+#'
+#' A synthetic dataset containing the closing prices and other attributes of 38
+#'  power futures contracts. The variables are as follows:
+#'
+#' @format A data frame with 38 rows and 5 columns:
+#' \describe{
+#'   \item{Include}{boolean variable to determine if contract should be included in forward curve calculation}
+#'   \item{Contract}{the name of the futures contract}
+#'   \item{Start}{delivery start date for the futures contract}
+#'   \item{End}{delivery start date for the futures contract}
+#'   \item{Closing}{the futures contract closing price}
+#' }
+"powfutures130513"
+
+
+#' Historical daily closing prices for 11 calendar year power futures contracts
+#'
+#' A synthetic dataset containing the closing prices and other attributes of 11
+#'  power futures contracts for calendar year delivery for 2006 - 2016. The variables are as follows:
+#'
+#' @format A data frame with 3253 rows and 12 columns:
+#' \describe{
+#'   \item{Date}{the trading date}
+#'   \item{CAL-06}{the closing price for the 20016 futures contract}
+#'   \item{CAL-07}{the closing price for the 2007 futures contract}
+#'   \item{CAL-08}{the closing price for the 2008 futures contract}
+#'   \item{CAL-09}{the closing price for the 2009 futures contract}
+#'   \item{CAL-10}{the closing price for the 2010 futures contract}
+#'   \item{CAL-11}{the closing price for the 2011 futures contract}
+#'   \item{CAL-12}{the closing price for the 2012 futures contract}
+#'   \item{CAL-13}{the closing price for the 2013 futures contract}
+#'   \item{CAL-14}{the closing price for the 2014 futures contract}
+#'   \item{CAL-14}{the closing price for the 2015 futures contract}
+#'   \item{CAL-16}{the closing price for the 2016 futures contract}
+#' }
+"powcal"
