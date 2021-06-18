cd6101be6a30d0ff2876480695279fe17c05790c
sleire <sleire@gmail.com>
2021-06-18 11:48:25 +0200

    add etrm-package doc

---
+++ R/etrm-package.R
@@ -0,0 +1,12 @@
+#' @docType package
+#' @name etrm-package
+#' @title etrm: Energy Trading and Risk Management in R
+#' @description Tools for energy market risk management (forward curves and trading strategies)
+#' @author Anders D. Sleire <sleire@gmail.com>
+#' @references F. E. Benth, S. Koekkebakker, and F. Ollmar.  Extracting and applying smooth forward curves fromaverage-based commodity contracts with seasonal variation.The Journal of Derivatives, 15(1):52–66,2007b. URLhttps://doi.org/10.3905/jod.2007.694791
+#' @references F. E. Benth,  J. S. Benth,  and S. Koekebakker.Stochastic modelling of electricity and related markets,volume 11. World Scientific, 2008. URLhttps://doi.org/10.1142/6811
+#' @references F. Black. The pricing of commodity contracts.Journal of financial economics, 3(1):167–179, 1976. URLhttps://doi.org/10.1016/0304-405X(76)90024-6
+#' @references T. Bjork.Arbitrage Theory in Continuous Time.  Oxford University Press, 3 edition, 2009.  URLhttps://EconPapers.repec.org/RePEc:oxp:obooks:9780199574742
+#' @references F. Black and R. W. Jones. Simplifying portfolio insurance.The Journal of Portfolio Management, 14(1):48–51, 1987. URLhttps://doi.org/10.3905/jpm.1987.409131
+#' @references H. E. Leland. Who should buy portfolio insurance?The Journal of Finance, 35(2):581–594, 1980. URLhttp://www.jstor.org/stable/2327419
+NULL
